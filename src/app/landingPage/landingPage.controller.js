5272e275853900e4520bf126d6931ae45c112178
Jonathon Mascorella <joe_mascorella@me.com>
2016-11-29 12:53:02 +1100

    Demonstrate Firebase object syncing

--- src/app/landingPage/landingPage.controller.js
+++ src/app/landingPage/landingPage.controller.js
@@ -1,6 +1,22 @@
-angular.module('myApp').controller('landingPageController', ['$scope', function($scope) {
+angular.module('myApp').controller('landingPageController', ['$scope', '$timeout', 'localFirebaseService', function($scope, $timeout, localFirebaseService) {
 	
 	$scope.salutation = "Hello there, world!";
     $scope.someOther = "There is nothing here";
-    
+	$scope.userPrefBtnTitle = "Test save";
+	
+    $scope.saveUserData = function() {
+		$scope.userPrefBtnTitle = "Saving...";
+		
+    	localFirebaseService.setCurrentUserData('/userPrefs', {
+    		panelsOpen: 'none',
+    	}).then(function(result) {
+			
+    		$scope.userPrefBtnTitle = "Saved!";
+			$timeout(function() { $scope.userPrefBtnTitle = "Test save"; }, 1000);
+			
+    	}, function(error) {
+    		$scope.userPrefBtnTitle = error;
+    	});
+    }
+	
 }]);
